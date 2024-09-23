1f49e1c48fc34da8f200639ade786b0b5c316f6d
azvmari <202111263@uesb.edu.br>
2024-09-22 23:22:06 -0300

    pagina de alimentaçao

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-document.getElementById("form-calculate-imc").addEventListener("submit", calculateIMC)
+const form = document.getElementById("form-calculate-imc")
+if(form) {
+  form.addEventListener("submit", calculateIMC)
+}
+
+(function constructMenu() {
+  document.body.innerHTML += `
+  <div id="menu-expansivo">
+    <button onclick="toggleMenu()"> 
+      <i class="fa-solid fa-bars"></i>
+    </button>
+    <a href="/index.html">Início</a>
+    <a href="/alimentacao.html">Alimentação</a>
+    <a>Exercícios</a>
+    <a>Saúde mental</a>
+    <a href="/calculadoraIMC.html">Calculadora de IMC</a>
+  </div>
+  `
+})()
 
 function toggleMenu() {
   const menu = document.getElementById("menu-expansivo")
-  if (menu.className === "open"){
+  if (menu.className === "open") {
     menu.className = ""
   }
   else {
@@ -10,7 +28,7 @@ function toggleMenu() {
   }
 }
 
-function calculateIMC(event){
+function calculateIMC(event) {
   event.preventDefault()
 
   const inputWeight = document.getElementById("input-weight")
@@ -20,22 +38,22 @@ function calculateIMC(event){
   const weight = parseFloat(inputWeight.value)
   const height = parseFloat(inputHeight.value)
 
-  if(isNaN(weight) || isNaN(height)) {
+  if (isNaN(weight) || isNaN(height)) {
     resultText.innerText = ""
   }
 
   const result = weight / (height * height)
   let classification = ""
 
-  if(result < 18.5) {
+  if (result < 18.5) {
     classification = "abaixo do peso"
-  } else if(result < 25) {
+  } else if (result < 25) {
     classification = "com peso normal"
-  } else if(result < 30) {
+  } else if (result < 30) {
     classification = "em sobrepeso"
-  } else if(result < 35) {
+  } else if (result < 35) {
     classification = "em obesidade grau 1"
-  } else if(result < 40){
+  } else if (result < 40) {
     classification = "em obesidade grau 2"
   } else {
     classification = "em obesidade grau 3"
